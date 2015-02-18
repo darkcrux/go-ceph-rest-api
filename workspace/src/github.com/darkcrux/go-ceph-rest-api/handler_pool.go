774f1503057e0198df7ee711cbc31336c4314fc5
Leo Martin Villasica <lvillasica@lvillasica-Vostro-3550.(none)>
2015-02-18 11:32:11 +0800

    Update naming.

--- workspace/src/github.com/darkcrux/go-ceph-rest-api/handler_pool.go
+++ workspace/src/github.com/darkcrux/go-ceph-rest-api/handler_pool.go
@@ -11,15 +11,15 @@ import (
 
 const (
 	// actions
-	create = "create"
-	// get = "get"
-	// delete = "delete"
+	ActionCreate = "create"
+	// ActionGet = "get"
+	// ActionDelete = "delete"
 
 	// methods
-	// get = "GET"
-	// post = "POST"
-	put = "PUT"
-	// delete = "DELETE"
+	// MethodGet = "GET"
+	// MethodPost = "POST"
+	MethodPut = "PUT"
+	// MethodDelete = "DELETE"
 )
 
 type poolAPI struct {
@@ -41,19 +41,27 @@ func poolHandler(res http.ResponseWriter, req *http.Request) {
 		json_response, _ := json.Marshal(resp)
 		fmt.Fprint(res, string(json_response))
 	}
-	pool.handlePut(callback)
-	// pool.handlePost(callback)
-	// pool.handleGet(callback)
-	// pool.handleDelete(callback)
+
+	switch req.Method {
+	default:
+		res.WriteHeader(204)               // Default; No Content
+		fmt.Fprint(res, "204: No Content") // Will replace this later
+	case MethodPut:
+		pool.handlePut(callback)
+		// case MethodPost:
+		// 	pool.handlePost(callback)
+		// case MethodGet:
+		// 	pool.handleGet(callback)
+		// case MethodDelete:
+		// 	pool.handleDelete(callback)
+	}
 }
 
 func (pool poolAPI) handlePut(callback func(resp Response)) {
-	if action := pool.Params["action"]; pool.Req.Method == put {
-		switch action {
-		case create:
-			resp := pool.create()
-			callback(resp)
-		}
+	switch pool.Params["action"] {
+	case ActionCreate:
+		resp := pool.create()
+		callback(resp)
 	}
 }
 
