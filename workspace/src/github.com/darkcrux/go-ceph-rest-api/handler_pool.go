8b67678c14eb50488911d3f32db49e95568793bf
Leo Martin Villasica <lvillasica@lvillasica-Vostro-3550.(none)>
2015-02-18 11:16:05 +0800

    stick status code to Response struct

--- workspace/src/github.com/darkcrux/go-ceph-rest-api/handler_pool.go
+++ workspace/src/github.com/darkcrux/go-ceph-rest-api/handler_pool.go
@@ -31,11 +31,13 @@ type poolAPI struct {
 type Response struct {
 	Status string      `json:"status"`
 	Output interface{} `json:"output"`
+	Code   int         `json:code`
 }
 
 func poolHandler(res http.ResponseWriter, req *http.Request) {
 	pool := poolAPI{res, req, mux.Vars(req)}
 	callback := func(resp Response) {
+		res.WriteHeader(resp.Code)
 		json_response, _ := json.Marshal(resp)
 		fmt.Fprint(res, string(json_response))
 	}
@@ -59,10 +61,10 @@ func (pool poolAPI) create() (resp Response) {
 	poolName := pool.Req.URL.Query().Get("pool")
 	if err := ceph.CreatePool(poolName); err != nil {
 		resp.Status = err.Error()
-		pool.Res.WriteHeader(400)
+		resp.Code = 400
 	} else {
 		resp.Status = fmt.Sprintf("pool '%s' created.", poolName)
-		pool.Res.WriteHeader(201)
+		resp.Code = 201
 	}
 	resp.Output = []string{}
 	return
